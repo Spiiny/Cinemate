65573ade8e90a7b030b522c530d16ea6d6240d05
Jayanth <jayanthpravin.rp@gmail.com>
2025-07-15 15:55:03 +0530

    Fixed API key usage with .env in UseFetch

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -2,8 +2,9 @@ import { useState,useEffect } from "react";
 
 export const UseFetch = (apiPath,queryTerm="") => {
 
+    const apiKey = import.meta.env.VITE_API_KEY;
     const [data, setData] = useState([]);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=b7a50a2c94bcad543a7e1cba7c4e0555&query=${queryTerm}`;
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${apiKey}&query=${queryTerm}`;
 
     useEffect(()=>{
     async function fetches(){
